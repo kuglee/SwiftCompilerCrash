d787811535567d471627b8873b3b3809b50d4c64
Gábor Librecz <kuglee@gmail.com>
2024-01-29 19:50:00 +0100

    Add TCA

--- Package.swift
+++ Package.swift
@@ -1,21 +1,25 @@
 // swift-tools-version: 5.10
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "SwiftCompilerCrash",
+    platforms: [.iOS(.v17), .macOS(.v14), .tvOS(.v17), .watchOS(.v10)],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "SwiftCompilerCrash",
             targets: ["SwiftCompilerCrash"]),
     ],
+    dependencies: [
+      .package(url: "https://github.com/pointfreeco/swift-composable-architecture", from: "1.0.0"),
+    ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "SwiftCompilerCrash"),
+            name: "SwiftCompilerCrash",
+            dependencies: [
+              .product(name: "ComposableArchitecture", package: "swift-composable-architecture"),
+            ]
+        ),
         .testTarget(
             name: "SwiftCompilerCrashTests",
             dependencies: ["SwiftCompilerCrash"]),
